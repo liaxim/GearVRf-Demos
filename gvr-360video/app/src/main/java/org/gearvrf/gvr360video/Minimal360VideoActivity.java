eda8f3e526908194a3f04d92ab4be7f3f52839ec
Mihail Marinov <m.marinov@samsung.com>
2016-10-18 09:24:40 -0700

    .

--- gvr-360video/app/src/main/java/org/gearvrf/gvr360video/Minimal360VideoActivity.java
+++ gvr-360video/app/src/main/java/org/gearvrf/gvr360video/Minimal360VideoActivity.java
@@ -16,22 +16,44 @@
 package org.gearvrf.gvr360video;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
 import android.media.MediaCodec;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Surface;
 
+import com.google.android.exoplayer.DefaultLoadControl;
 import com.google.android.exoplayer.ExoPlaybackException;
 import com.google.android.exoplayer.ExoPlayer;
+import com.google.android.exoplayer.LoadControl;
 import com.google.android.exoplayer.MediaCodecAudioTrackRenderer;
 import com.google.android.exoplayer.MediaCodecSelector;
+import com.google.android.exoplayer.MediaCodecTrackRenderer;
 import com.google.android.exoplayer.MediaCodecVideoTrackRenderer;
+import com.google.android.exoplayer.TimeRange;
+import com.google.android.exoplayer.TrackRenderer;
+import com.google.android.exoplayer.audio.AudioCapabilities;
+import com.google.android.exoplayer.audio.AudioTrack;
+import com.google.android.exoplayer.chunk.ChunkSampleSource;
+import com.google.android.exoplayer.chunk.ChunkSource;
+import com.google.android.exoplayer.chunk.Format;
+import com.google.android.exoplayer.chunk.FormatEvaluator;
+import com.google.android.exoplayer.dash.DashChunkSource;
+import com.google.android.exoplayer.dash.DefaultDashTrackSelector;
+import com.google.android.exoplayer.dash.mpd.MediaPresentationDescriptionParser;
 import com.google.android.exoplayer.extractor.ExtractorSampleSource;
 import com.google.android.exoplayer.upstream.AssetDataSource;
+import com.google.android.exoplayer.upstream.BandwidthMeter;
+import com.google.android.exoplayer.upstream.DataSource;
 import com.google.android.exoplayer.upstream.DataSpec;
 import com.google.android.exoplayer.upstream.DefaultAllocator;
+import com.google.android.exoplayer.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer.upstream.DefaultUriDataSource;
+import com.google.android.exoplayer.util.ManifestFetcher;
 
 import org.gearvrf.GVRActivity;
 import org.gearvrf.scene_objects.GVRVideoSceneObject;
@@ -115,17 +137,186 @@ public class Minimal360VideoActivity extends GVRActivity {
         return GVRVideoSceneObject.makePlayerInstance(mediaPlayer);
     }
 
+    Handler mainHandler;
     private GVRVideoSceneObjectPlayer<ExoPlayer> makeExoPlayer() {
         final ExoPlayer player = ExoPlayer.Factory.newInstance(2);
 
-        final AssetDataSource dataSource = new AssetDataSource(this);
-        final ExtractorSampleSource sampleSource = new ExtractorSampleSource(Uri.parse("asset:///videos_s_3.mp4"),
-                dataSource, new DefaultAllocator(64 * 1024), 64 * 1024 * 256);
+        MediaPresentationDescriptionParser parser = new MediaPresentationDescriptionParser();
+        final String userAgent = "GVRF 3.x";
+        final Context context = this;
+        DefaultUriDataSource manifestDataSource = new DefaultUriDataSource(context, userAgent);
+        final String url = "http://www.youtube.com/api/manifest/dash/id/bf5bb2419360daf1/source/youtube?as=fmp4_audio_clear,fmp4_sd_hd_clear&sparams=ip,ipbits,expire,source,id,as&ip=0.0.0.0&ipbits=0&expire=19000000000&signature=51AF5F39AB0CEC3E5497CD9C900EBFEAECCCB5C7.8506521BFC350652163895D4C26DEE124209AA9E&key=ik0";
+        ManifestFetcher manifestFetcher = new ManifestFetcher<>(url, manifestDataSource, parser);
 
-        final MediaCodecVideoTrackRenderer videoRenderer = new MediaCodecVideoTrackRenderer(this, sampleSource,
-                MediaCodecSelector.DEFAULT, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT);
-        final MediaCodecAudioTrackRenderer audioRenderer = new MediaCodecAudioTrackRenderer(sampleSource,
-                MediaCodecSelector.DEFAULT);
+        mainHandler = new Handler();
+        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter(mainHandler, new BandwidthMeter.EventListener() {
+            @Override
+            public void onBandwidthSample(int elapsedMs, long bytes, long bitrate) {
+            }
+        });
+
+        DataSource videoDataSource = new DefaultUriDataSource(context, userAgent);
+        ChunkSource videoChunkSource = new DashChunkSource(manifestFetcher,
+                DefaultDashTrackSelector.newVideoInstance(context, true, false),
+                videoDataSource, new FormatEvaluator.AdaptiveEvaluator(bandwidthMeter), 30000,
+                0L, mainHandler, new DashChunkSource.EventListener() {
+            @Override
+            public void onAvailableRangeChanged(int sourceId, TimeRange availableRange) {
+
+            }
+        }, 0);
+        ChunkSampleSource videoSampleSource = new ChunkSampleSource(videoChunkSource,
+                new DefaultLoadControl(new DefaultAllocator(64 * 1024)),
+                200 * 64 * 1024, mainHandler, new ChunkSampleSource.EventListener() {
+            @Override
+            public void onLoadStarted(int sourceId, long length, int type, int trigger, Format format, long mediaStartTimeMs, long mediaEndTimeMs) {
+
+            }
+
+            @Override
+            public void onLoadCompleted(int sourceId, long bytesLoaded, int type, int trigger, Format format, long mediaStartTimeMs, long mediaEndTimeMs, long elapsedRealtimeMs, long loadDurationMs) {
+
+            }
+
+            @Override
+            public void onLoadCanceled(int sourceId, long bytesLoaded) {
+
+            }
+
+            @Override
+            public void onLoadError(int sourceId, IOException e) {
+
+            }
+
+            @Override
+            public void onUpstreamDiscarded(int sourceId, long mediaStartTimeMs, long mediaEndTimeMs) {
+
+            }
+
+            @Override
+            public void onDownstreamFormatChanged(int sourceId, Format format, int trigger, long mediaTimeMs) {
+
+            }
+        }, 0);
+        final TrackRenderer videoRenderer = new MediaCodecVideoTrackRenderer(context, videoSampleSource,
+                MediaCodecSelector.DEFAULT, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT, 5000,
+                null, true, mainHandler, new MediaCodecVideoTrackRenderer.EventListener() {
+            @Override
+            public void onDroppedFrames(int count, long elapsed) {
+
+            }
+
+            @Override
+            public void onVideoSizeChanged(int width, int height, int unappliedRotationDegrees, float pixelWidthHeightRatio) {
+
+            }
+
+            @Override
+            public void onDrawnToSurface(Surface surface) {
+
+            }
+
+            @Override
+            public void onDecoderInitializationError(MediaCodecTrackRenderer.DecoderInitializationException e) {
+
+            }
+
+            @Override
+            public void onCryptoError(MediaCodec.CryptoException e) {
+
+            }
+
+            @Override
+            public void onDecoderInitialized(String decoderName, long elapsedRealtimeMs, long initializationDurationMs) {
+
+            }
+        }, 50);
+
+        // Build the audio renderer.
+        DataSource audioDataSource = new DefaultUriDataSource(context, bandwidthMeter, userAgent);
+        ChunkSource audioChunkSource = new DashChunkSource(manifestFetcher,
+                DefaultDashTrackSelector.newAudioInstance(), audioDataSource, null, 30000,
+                0L, mainHandler, new DashChunkSource.EventListener() {
+            @Override
+            public void onAvailableRangeChanged(int sourceId, TimeRange availableRange) {
+
+            }
+        }, 1);
+        ChunkSampleSource audioSampleSource = new ChunkSampleSource(audioChunkSource,
+                new DefaultLoadControl(new DefaultAllocator(64 * 1024)),
+                54 * 64 * 1024, mainHandler, new ChunkSampleSource.EventListener() {
+            @Override
+            public void onLoadStarted(int sourceId, long length, int type, int trigger, Format format, long mediaStartTimeMs, long mediaEndTimeMs) {
+
+            }
+
+            @Override
+            public void onLoadCompleted(int sourceId, long bytesLoaded, int type, int trigger, Format format, long mediaStartTimeMs, long mediaEndTimeMs, long elapsedRealtimeMs, long loadDurationMs) {
+
+            }
+
+            @Override
+            public void onLoadCanceled(int sourceId, long bytesLoaded) {
+
+            }
+
+            @Override
+            public void onLoadError(int sourceId, IOException e) {
+
+            }
+
+            @Override
+            public void onUpstreamDiscarded(int sourceId, long mediaStartTimeMs, long mediaEndTimeMs) {
+
+            }
+
+            @Override
+            public void onDownstreamFormatChanged(int sourceId, Format format, int trigger, long mediaTimeMs) {
+
+            }
+        }, 1);
+        TrackRenderer audioRenderer = new MediaCodecAudioTrackRenderer(audioSampleSource,
+                MediaCodecSelector.DEFAULT, null, true, mainHandler, new MediaCodecAudioTrackRenderer.EventListener() {
+            @Override
+            public void onAudioTrackInitializationError(AudioTrack.InitializationException e) {
+
+            }
+
+            @Override
+            public void onAudioTrackWriteError(AudioTrack.WriteException e) {
+
+            }
+
+            @Override
+            public void onAudioTrackUnderrun(int bufferSize, long bufferSizeMs, long elapsedSinceLastFeedMs) {
+
+            }
+
+            @Override
+            public void onDecoderInitializationError(MediaCodecTrackRenderer.DecoderInitializationException e) {
+
+            }
+
+            @Override
+            public void onCryptoError(MediaCodec.CryptoException e) {
+
+            }
+
+            @Override
+            public void onDecoderInitialized(String decoderName, long elapsedRealtimeMs, long initializationDurationMs) {
+
+            }
+        },
+                AudioCapabilities.getCapabilities(context), AudioManager.STREAM_MUSIC);
+
+//        final AssetDataSource dataSource = new AssetDataSource(this);
+//        final ExtractorSampleSource sampleSource = new ExtractorSampleSource(Uri.parse("asset:///videos_s_3.mp4"),
+//                dataSource, new DefaultAllocator(64 * 1024), 64 * 1024 * 256);
+//
+//        final MediaCodecVideoTrackRenderer videoRenderer = new MediaCodecVideoTrackRenderer(this, sampleSource,
+//                MediaCodecSelector.DEFAULT, MediaCodec.VIDEO_SCALING_MODE_SCALE_TO_FIT);
+//        final MediaCodecAudioTrackRenderer audioRenderer = new MediaCodecAudioTrackRenderer(sampleSource,
+//                MediaCodecSelector.DEFAULT);
         player.prepare(videoRenderer, audioRenderer);
 
         return new GVRVideoSceneObjectPlayer<ExoPlayer>() {
@@ -193,5 +384,5 @@ public class Minimal360VideoActivity extends GVRActivity {
 
     private GVRVideoSceneObjectPlayer<?> videoSceneObjectPlayer;
 
-    static final boolean USE_EXO_PLAYER = false;
+    static final boolean USE_EXO_PLAYER = true;
 }

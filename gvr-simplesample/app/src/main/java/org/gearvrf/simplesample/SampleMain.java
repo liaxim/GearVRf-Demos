9228d1b2238d7e69369b4538c5148b4239f5e70f
Mihail Marinov <m.marinov@samsung.com>
2017-04-03 10:05:01 -0700

    .

--- gvr-simplesample/app/src/main/java/org/gearvrf/simplesample/SampleMain.java
+++ gvr-simplesample/app/src/main/java/org/gearvrf/simplesample/SampleMain.java
@@ -15,24 +15,38 @@
 
 package org.gearvrf.simplesample;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.SurfaceTexture;
+import android.opengl.GLES30;
+import android.opengl.GLES31;
+import android.view.Surface;
 
 import org.gearvrf.GVRAndroidResource;
 import org.gearvrf.GVRCameraRig;
 import org.gearvrf.GVRContext;
+import org.gearvrf.GVRDrawFrameListener;
+import org.gearvrf.GVRExternalTexture;
 import org.gearvrf.GVRMain;
+import org.gearvrf.GVRMaterial;
 import org.gearvrf.GVRScene;
 import org.gearvrf.GVRSceneObject;
+import org.gearvrf.GVRShaderId;
+import org.gearvrf.GVRSharedTexture;
 import org.gearvrf.GVRTexture;
+import org.gearvrf.utility.Log;
+
+import static android.opengl.GLES20.glGenTextures;
 
 public class SampleMain extends GVRMain {
 
     private GVRContext mGVRContext;
 
     @Override
-    public void onInit(GVRContext gvrContext) {
+    public void onInit(final GVRContext gvrContext) {
 
-        // save context for possible use in onStep(), even though that's empty
+        // save context for possible use in onStep(), even though that'surface empty
         // in this sample
         mGVRContext = gvrContext;
 
@@ -51,7 +65,7 @@ public class SampleMain extends GVRMain {
 
         // create a scene object (this constructor creates a rectangular scene
         // object that uses the standard 'unlit' shader)
-        GVRSceneObject sceneObject = new GVRSceneObject(gvrContext, 4.0f, 2.0f,
+        final GVRSceneObject sceneObject = new GVRSceneObject(gvrContext, 4.0f, 2.0f,
                 texture);
 
         // set the scene object position
@@ -60,7 +74,62 @@ public class SampleMain extends GVRMain {
         // add the scene object to the scene graph
         scene.addSceneObject(sceneObject);
 
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                textureId = new int[1];
+                GLES30.glGenTextures(1, textureId, 0);
+                int err;
+                if (GLES31.GL_NO_ERROR != (err = GLES31.glGetError())) {
+                    Log.i("mmarinov", "glGenTextures error " + err);
+                }
+
+                surfaceTexture = new SurfaceTexture(textureId[0]);
+                surfaceTexture.setDefaultBufferSize(1024, 1024);
+                surface = new Surface(surfaceTexture);
+                surfaceTexture.setOnFrameAvailableListener(new SurfaceTexture.OnFrameAvailableListener() {
+                    @Override
+                    public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+                        Log.i("mrm_server", "onFrameAvailable ");
+                    }
+                });
+
+//            final SurfaceHolder holder = view.getHolder();
+                //final Surface surface = holder.getSurface();
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setComponent(new ComponentName("com.example.test.mrm_client", "com.example.test.mrm_client.MyIntentService"));
+                intent.putExtra("surface", surface);
+                gvrContext.getContext().startService(intent);
+
+                GVRSharedTexture texture = new GVRSharedTexture(gvrContext, textureId[0]);
+                sceneObject.getRenderData().getMaterial().setMainTexture(texture);
+                sceneObject.getRenderData().getMaterial().setTexture("diffuseTexture", texture);
+                sceneObject.getRenderData().getMaterial().setShaderType(GVRMaterial.GVRShaderType.OES.ID);
+                if (GLES31.GL_NO_ERROR != (err = GLES31.glGetError())) {
+                    Log.i("mmarinov", "run error " + err);
+                }
+
+                GVRDrawFrameListener listener = new GVRDrawFrameListener() {
+                    @Override
+                    public void onDrawFrame(float v) {
+                        int err;
+                        if (GLES31.GL_NO_ERROR != (err = GLES31.glGetError())) {
+                            Log.i("mmarinov", "run error1 " + err);
+                        }
+                        surfaceTexture.updateTexImage();
+                        if (GLES31.GL_NO_ERROR != (err = GLES31.glGetError())) {
+                            Log.i("mmarinov", "run error2 " + err);
+                        }
+                    }
+                };
+                gvrContext.registerDrawFrameListener(listener);
+            }
+        };
+        gvrContext.runOnGlThread(r);
     }
+    int[] textureId;
+    SurfaceTexture surfaceTexture;
+    Surface surface;
 
     @Override
     public void onStep() {
